37f4254077878f1d5107c6d10687af7c4f56ed8b
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-04 13:26:43 -0300

    add alt on logo component, and import news logo images

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -3,8 +3,11 @@ import { Logo } from '../atoms/Logo'
 
 const Header = () => {
   return (
-      <header className="h-1/5 bg-orange-800 shadow-md p-5 md:px-10 transform">
-          <Logo url="/images/chuck-logo.png"/>
+      <header className="h-1/6 bg-neutral-100 shadow-lg p-5 md:px-10 transform">
+        <Logo 
+            url="/images/chuck-logo.png" 
+            alt="chuck facts logo with chucknorris silhouette in the center"
+        />
       </header>
   )
 }
