71b87ee58f2f04a02cf963f88499f0f84e87cc23
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-04 13:25:40 -0300

    Create a template component home page to address responsibilities with singularity

---
+++ templates/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Background } from '../components/atoms/Background'
+import { Header } from '../components/molecules/Header'
+
+const HomeTemplate = () => {
+  return (
+    <Background>
+        <Header/>
+    </Background>
+  )
+}
+
+export { HomeTemplate }
