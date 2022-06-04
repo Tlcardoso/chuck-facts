fad55f3ef6f50a409d05677ee7bee0fb9d95254f
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-04 15:14:29 -0300

    Add new logo

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
       <header className="h-1/6 bg-neutral-100 shadow-lg p-5 md:px-10 transform">
         <Logo 
-            url="/images/chuck-logo.png" 
+            url="/images/chuck-logo-big.png" 
             alt="chuck facts logo with chucknorris silhouette in the center"
         />
       </header>
