29ca9f24bc7446663d6ac5f8f3e25d62675f3c3d
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-05 09:46:14 -0300

    feat: create a api connections

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+const BASE_URL = 'https://api.chucknorris.io/jokes'
+
+export const getFact = async () => {
+  const fact = await fetch(`${BASE_URL}/random`)
+    .then((response) => response.json())
+
+  return fact
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export const getFactByCategory = async (category: string) => {
+  const fact = await fetch(`${BASE_URL}/random?category=${category}`)
+    .then((response) => response.json())
+
+  return fact
+}
+
+export const getFactByQuery = async (query: string) => {
+  const fact = await fetch(`${BASE_URL}/random?query=${query}`)
+    .then((response) => response.json())
+
+  return fact
 }
