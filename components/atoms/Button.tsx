72c7d8b030d6f87005b6be23900cd6a2e46db8fc
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-05 08:53:38 -0300

    feat: Create Button

---
+++ components/atoms/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface ButtonProps {
+    children: string
+    onClick?: () => void
+}
+
+const Button = ({children, onClick}: ButtonProps) => {
+  return (
+    <button 
+        onClick={onClick} 
+        className="text-orange-800 
+            bg-white
+            px-5 md:px-10
+            py-2 md:py-4
+            shadow-md
+            rounded-xl
+            font-bold my-3
+            hover:shadow-xl
+            active:scale-90
+            transition
+            duration-150"
+    >
+        {children}
+    </button>
+  )
+}
+
+export { Button }
