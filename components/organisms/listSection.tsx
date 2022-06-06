35c2e578b1f093af6171e031104d9a6aa0270331
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-05 23:24:02 -0300

    feat: configure and create props to ListSection component

--- components/organisms/listSection.tsx
+++ components/organisms/listSection.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import { Title } from '../atoms/Title'
+import { FactListCard } from '../atoms/FactListCard'
 
-const ListSection = () => {
+interface Fact {
+  icon_url: string
+  id: string
+  url: string
+  value: string
+  updated_at: string
+}
+interface ListSectionProps {
+  facts: Fact[]
+}
+
+const ListSection = ({facts}: ListSectionProps) => {
+
   return (
-    <div className="w-full bg-orange-800 rounded flex">
+    <div className="w-full bg-orange-800 rounded-xl flex flex-col items-center justify-center text-white">
         <Title>List of Facts</Title>
+
+        <div className='w-full px-[2%] max-h-[400px] overflow-y-scroll scrollbar'>
+          {
+            facts?.map(({value, id, updated_at}) => (
+              <FactListCard 
+                url={'/images/chuck-norris-smile.png'} 
+                fact={value} 
+                alt={'picture of chuck norris smiling'} 
+                key={id}
+                upadateDate={updated_at}
+              />
+            ))
+          }
+        </div>
     </div>
   )
 }
