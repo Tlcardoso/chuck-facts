6a52f2bb56a4db84404c20f2ae340ef0fe6e4d3a
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-05 22:15:46 -0300

    feat: create a factList components

--- components/molecules/Facts.tsx
+++ components/molecules/Facts.tsx
@@ -11,9 +11,9 @@ interface FactsProps {
 
 const FactsPhoto = ({ url, alt, children, onClick}: FactsProps) => {
   return (
-    <div className='h-[95%] w-full lg:w-[70%] rounded-2xl relative border-8 border-orange-800 text-white'>
+    <div className='h-[475px] w-full lg:w-[70%] rounded-2xl relative border-8 border-orange-800 text-white '>
 
-        <div className="absolute mt-4 text-center w-full bg-orange-800 z-10">
+        <div className="absolute z-10 mt-4 text-center w-full bg-orange-800">
             <Title>Facts</Title>
             <BsFillArrowRightSquareFill onClick={onClick} className=" absolute w-10 h-10 right-6 bottom-3 cursor-pointer"/>
         </div>
@@ -24,7 +24,7 @@ const FactsPhoto = ({ url, alt, children, onClick}: FactsProps) => {
             className="w-full h-full rounded-lg brightness-50 object-cover"
         />
 
-        <p className='absolute w-full bottom-[30%] text-xl font-semibold text-center px-[5%]'>
+        <p className='absolute w-full bottom-[5%] text-sm sm:bottom-[30%] sm:text-xl font-semibold text-center px-[5%]'>
             {children}
         </p>
     </div>
---
+++ components/organisms/listSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Title } from '../atoms/Title'
+
+const ListSection = () => {
+  return (
+    <div className="w-full bg-orange-800 rounded flex">
+        <Title>List of Facts</Title>
+    </div>
+  )
+}
+
+export { ListSection }
