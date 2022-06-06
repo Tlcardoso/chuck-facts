66bdef2d8b777f3aa4715daabbb472b81d622810
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-05 23:23:28 -0300

    fix: add overflow styles on background

--- components/atoms/Background.tsx
+++ components/atoms/Background.tsx
@@ -6,7 +6,7 @@ interface BackgroundProps {
 
 const Background = ({children}: BackgroundProps ) => {
   return (
-    <div className="w-screen h-screen bg-slate-200">{children}</div>
+    <div className="w-screen h-screen bg-slate-200 overflow-x-hidden">{children}</div>
   )
 }
 
