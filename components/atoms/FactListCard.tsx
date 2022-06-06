7cfdfc32c3b6dadca83a20629b23fa823699bec4
Tlcardoso <tl.cardosogit@gmail.com>
2022-06-06 10:53:06 -0300

    feat: add close button on FactListCard

--- components/atoms/FactListCard.tsx
+++ components/atoms/FactListCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 interface factListCardProps {
   url: string
@@ -8,6 +9,12 @@ interface factListCardProps {
 }
 
 const FactListCard = ({url, alt, fact, upadateDate}: factListCardProps) => {
+  const [closed, setClosed] = useState(false)
+
+
+  const handleClose = () => {
+    setClosed(true)
+  }
 
   const dateFormat = (date: string) => {
     const event = new Date(date)
@@ -17,7 +24,7 @@ const FactListCard = ({url, alt, fact, upadateDate}: factListCardProps) => {
   const date = dateFormat(upadateDate)
 
   return (
-    <div className='w-full h-auto bg-white rounded-full flex items-center mb-4'>
+    <div className={closed ? `hidden` : `w-full h-auto bg-white rounded-full flex items-center relative mb-4`}>
       <img 
         className='h-[70px] w-[70px] m-2 rounded-full object-cover border-4 border-orange-800' 
         src={url} 
@@ -28,6 +35,11 @@ const FactListCard = ({url, alt, fact, upadateDate}: factListCardProps) => {
         <p>{fact}</p>
         <p className='font-bold text-orange-900'>{date}</p>
       </div>
+
+      <AiFillCloseCircle 
+        className='absolute right-5 h-[15px] w-[15px] sm:h-[30px] sm:w-[30px] cursor-pointer text-orange-900'
+        onClick={handleClose}
+      />
     </div>
   )
 }
